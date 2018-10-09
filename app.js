affebe3c5eeb0724fdf7381a65cb7eada953f1cf
DPGoertzen <dpgoertzen@gmail.com>
2018-10-08 23:32:59 -0500

    basic crud node app

--- app.js
+++ app.js
@@ -2,5 +2,41 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv
+var command = argv._[0];
+
+console.log(`Command: ${command}`);
+
+if (command === 'add'){
+  var note = notes.addNote(argv.title, argv.body);
+  if (note){
+    console.log(
+      `Note successfully created.
+      Title: ${note.title}
+      Body: ${note.body}`
+    );
+  } else {
+    console.log("Unable to create note!")
+  }
+} else if (command === 'list'){
+  notes.getAll();
+} else if (command === 'read'){
+  var singleNote = notes.getOne(argv.title);
+  if (singleNote){
+    console.log("The note you were looking for:");
+    console.log(`Title: ${singleNote.title}`);
+    console.log(`Body: ${singleNote.body}`);
+  } else {
+    console.log("No notes exist with that title");
+  }
+} else if (command === 'remove'){
+  var noteRemoved = notes.deleteOne(argv.title);
+  var message = noteRemoved ? "Note was removed" : "Note was not able to be removed";
+  console.log(message);
+} else {
+  console.log('command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,6 +1,66 @@
 console.log('starting notes.js')
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'newnote'
-}
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (err){
+    console.log(`error: ${err}`);
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+  // console.log('adding note', title, body);
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0){
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var getAll = () => {
+  var allNotes = fetchNotes();
+  console.log("All current notes:");
+  for(i=0; i < allNotes.length; i++){
+    console.log(allNotes[i].title);
+  }
+  return allNotes;
+};
+
+var getOne = (title) => {
+  var allNotes = fetchNotes();
+  var returnedNote = allNotes.filter((note) => note.title === title)[0];
+  if(returnedNote){
+    return returnedNote;
+  } else return false;
+};
+
+var deleteOne = (title) => {
+  var allNotes = fetchNotes();
+  var notesAfterDeletion = allNotes.filter((note) => note.title !== title);
+  saveNotes(notesAfterDeletion);
+
+  return allNotes.length !== notesAfterDeletion.length;
+};
+
+module.exports = {
+// in ES6 we don't need to give the key value pair since they are the same
+  addNote,
+  getAll,
+  getOne,
+  deleteOne
+};
